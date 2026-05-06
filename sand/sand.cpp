6e5e2bc875cc9587b1d5b0c53f74a90e987adf0b
Alxay <alxay2137@gmail.com>
2026-05-06 11:40:30 +0200

    New engine

--- sand/sand.cpp
+++ sand/sand.cpp
@@ -2,82 +2,182 @@
 #include <vector>
 
 
-class Sand {
-public:
-    int posX;
-    int posY;
-
-
-    Sand(int x, int y) {
-        this->posX = x;
-        this->posY = y;
-    }
-
-    void updatePos(int drX, int drY) {
-        this->posX += drX;
-        this->posY += drY;
-    }
+enum ParticleType {
+	EMPTY,
+    SAND,
+    WATER,
+    WALL
 };
 
+struct Cell {
+    ParticleType type;
+    bool updated; 
+};
 
-class Map {
-private:
-	int grid[800][600];
+//class Sand {
+//public:
+//    int posX;
+//    int posY;
+//
+//
+//    Sand(int x, int y) {
+//        this->posX = x;
+//        this->posY = y;
+//    }
+//
+//    void updatePos(int drX, int drY) {
+//        this->posX += drX;
+//        this->posY += drY;
+//    }
+//};
+//
 
+//class Map {
+//private:
+//	int grid[800][600];
+//
+//
+//public:
+   // std::vector<Sand> sandParticles;
+   // Map(){
+   // for (int x = 0; x < 800; x++) {
+   //     for (int y = 0; y < 600; y++) {
+   //         grid[x][y] = 0; // 0 for empty, 1 for sand
+
+   //         if (y > 590) {
+			//	grid[x][y] = 2; // 2 for solid ground
+   //         }
+   //         if (x < 10) {
+   //             grid[x][y] = 2; // 2 for solid ground
+			//}
+   //         if (x > 790) {
+   //             grid[x][y] = 2; // 2 for solid ground
+   //         }
+   //     }
+   // }
+   // }
+
+
+//    void calculateFrame() {
+//        for (auto& sand : sandParticles) {
+//            this->grid[sand.posX][sand.posY] = 0;
+//
+//            if (grid[sand.posX][sand.posY + 1] == 0) {
+//				sand.posY += 1;
+//			}
+//            else if (grid[sand.posX - 1][sand.posY + 1] == 0) {
+//                sand.posX -= 1;
+//            }else if (grid[sand.posX + 1][sand.posY + 1] == 0) {
+//                sand.posX += 1;
+//            }
+//            else {
+//
+//                this->grid[sand.posX][sand.posY] = 1;
+//
+//
+//            }
+//            this->grid[sand.posX][sand.posY] = 1;
+//           
+//		}
+//    }
+//};
+
+
+
+
+struct Map {
+//private:
 
 public:
-    std::vector<Sand> sandParticles;
-    Map(){
-    for (int x = 0; x < 800; x++) {
-        for (int y = 0; y < 600; y++) {
-            grid[x][y] = 0; // 0 for empty, 1 for sand
+	Cell grid[800][600];
 
-            if (y > 590) {
-				grid[x][y] = 2; // 2 for solid ground
-            }
-            if (x < 10) {
-                grid[x][y] = 2; // 2 for solid ground
+	Map() {
+		for (int i = 0; i < 800; i++) {
+			for (int j = 0; j < 600; j++) {
+				grid[i][j].type = EMPTY;
+				grid[i][j].updated = false;
+				if (j > 590) {
+					grid[i][j].type = WALL;
+				}
+				if (i < 10) {
+					grid[i][j].type = WALL;
+				}
+				if (i > 790) {
+					grid[i][j].type = WALL;
+				}
 			}
-            if (x > 790) {
-                grid[x][y] = 2; // 2 for solid ground
-            }
-        }
-    }
-    }
-
+		}
+	};
 
     void calculateFrame() {
-        for (auto& sand : sandParticles) {
-            this->grid[sand.posX][sand.posY] = 0;
+		for (int i = 800; i > 0; i--) {
+			for (int j = 600; j > 0; j--) {
+				if (grid[i][j].type == SAND && !grid[i][j].updated) {
+					if (grid[i][j + 1].type == EMPTY) {
+						grid[i][j + 1].type = SAND;
+						grid[i][j].type = EMPTY;
+						grid[i][j + 1].updated = true;
+					}
+					else if (grid[i - 1][j + 1].type == EMPTY) {
+						grid[i - 1][j + 1].type = SAND;
+						grid[i][j].type = EMPTY;
+						grid[i - 1][j + 1].updated = true;
+					}
+					else if (grid[i + 1][j + 1].type == EMPTY) {
+						grid[i + 1][j + 1].type = SAND;
+						grid[i][j].type = EMPTY;
+						grid[i + 1][j + 1].updated = true;
+					}
+					else if (grid[i][j + 1].type == WATER) {
+						grid[i][j + 1].type = SAND;
+						grid[i][j].type = WATER;
+						grid[i][j + 1].updated = true;
+					}
+				}
+				else if (grid[i][j].type == WATER && !grid[i][j].updated) {
+					if (grid[i][j + 1].type == EMPTY) {
+						grid[i][j + 1].type = WATER;
+						grid[i][j].type = EMPTY;
+						grid[i][j + 1].updated = true;
+					}
+					else if (grid[i - 1][j + 1].type == EMPTY) {
+						grid[i - 1][j + 1].type = WATER;
+						grid[i][j].type = EMPTY;
+						grid[i - 1][j + 1].updated = true;
+					}
+					else if (grid[i + 1][j + 1].type == EMPTY) {
+						grid[i + 1][j + 1].type = WATER;
+						grid[i][j].type = EMPTY;
+						grid[i + 1][j + 1].updated = true;
+					}
+					else if (grid[i - 1][j].type == EMPTY) {
+						grid[i - 1][j].type = WATER;
+						grid[i][j].type = EMPTY;
+						grid[i - 1][j].updated = true;
+					}
+					else if (grid[i + 1][j].type == EMPTY) {
+						grid[i + 1][j].type = WATER;
+						grid[i][j].type = EMPTY;
+						grid[i + 1][j].updated = true;
+					}
+				}
 
-            if (grid[sand.posX][sand.posY + 1] == 0) {
-				sand.posY += 1;
+				else {
+					grid[i][j].updated = false;
+				}
 			}
-            else if (grid[sand.posX - 1][sand.posY + 1] == 0) {
-                sand.posX -= 1;
-            }else if (grid[sand.posX + 1][sand.posY + 1] == 0) {
-                sand.posX += 1;
-            }
-            else {
-
-                this->grid[sand.posX][sand.posY] = 1;
-
-
-            }
-            this->grid[sand.posX][sand.posY] = 1;
-           
 		}
+    
     }
+
 };
-
-
-
 
 
 Map map = Map();
 int main() {
     InitWindow(800, 600, "Piasek");
-    int fps = 144;
+    int fps = 0;
+	SetTargetFPS(fps);
   
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -88,47 +188,85 @@ int main() {
 
         Vector2 mousePosition = GetMousePosition();
 
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+
+			for (int i = 0; i < 100; i++) {
+				map.grid[(int)mousePosition.x+i][(int)mousePosition.y].type = SAND;
+			}
+			//map.grid[(int)mousePosition.x][(int)mousePosition.y].type = SAND;
+			//map.grid[(int)mousePosition.x+1][(int)mousePosition.y].type = SAND;
+			//map.grid[(int)mousePosition.x][(int)mousePosition.y+1].type = SAND;
+			//map.grid[(int)mousePosition.x+1][(int)mousePosition.y+1].type = SAND;
+			//map.grid[(int)mousePosition.x-1][(int)mousePosition.y].type = SAND;
+			//map.grid[(int)mousePosition.x-1][(int)mousePosition.y+1].type = SAND;
+			//map.grid[(int)mousePosition.x+2][(int)mousePosition.y].type = SAND;
+			//map.grid[(int)mousePosition.x+2][(int)mousePosition.y+1].type = SAND;
 
 
-            Sand sand = Sand((int)mousePosition.x, (int)mousePosition.y);
-            Sand sand2 = Sand((int)mousePosition.x+1, (int)mousePosition.y);
-            Sand sand3 = Sand((int)mousePosition.x, (int)mousePosition.y+1);
-            Sand sand4 = Sand((int)mousePosition.x+1, (int)mousePosition.y+1);
-			Sand sand5 = Sand((int)mousePosition.x - 1, (int)mousePosition.y);
-			Sand sand6 = Sand((int)mousePosition.x - 1, (int)mousePosition.y + 1);
-			Sand sand7 = Sand((int)mousePosition.x + 2, (int)mousePosition.y);
-			Sand sand8 = Sand((int)mousePosition.x + 2, (int)mousePosition.y + 1);
-            
-			map.sandParticles.push_back(sand);
-			map.sandParticles.push_back(sand2);
-			map.sandParticles.push_back(sand3);
-			map.sandParticles.push_back(sand4);
-			map.sandParticles.push_back(sand5);
-			map.sandParticles.push_back(sand6);
-			map.sandParticles.push_back(sand7);
-			map.sandParticles.push_back(sand8);
+   //         Sand sand = Sand((int)mousePosition.x, (int)mousePosition.y);
+   //         Sand sand2 = Sand((int)mousePosition.x+1, (int)mousePosition.y);
+   //         Sand sand3 = Sand((int)mousePosition.x, (int)mousePosition.y+1);
+   //         Sand sand4 = Sand((int)mousePosition.x+1, (int)mousePosition.y+1);
+			//Sand sand5 = Sand((int)mousePosition.x - 1, (int)mousePosition.y);
+			//Sand sand6 = Sand((int)mousePosition.x - 1, (int)mousePosition.y + 1);
+			//Sand sand7 = Sand((int)mousePosition.x + 2, (int)mousePosition.y);
+			//Sand sand8 = Sand((int)mousePosition.x + 2, (int)mousePosition.y + 1);
+   //         
+			//map.sandParticles.push_back(sand);
+			//map.sandParticles.push_back(sand2);
+			//map.sandParticles.push_back(sand3);
+			//map.sandParticles.push_back(sand4);
+			//map.sandParticles.push_back(sand5);
+			//map.sandParticles.push_back(sand6);
+			//map.sandParticles.push_back(sand7);
+			//map.sandParticles.push_back(sand8);
 
-
-
-        }
-
-
-
-        float delta = GetFrameTime();
-        map.calculateFrame();
-
-      
-        for (auto& sand : map.sandParticles) {
-            DrawPixel(sand.posX, sand.posY, BLACK);
 		}
-		
+
+		//if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+		//	map.grid[(int)mousePosition.x][(int)mousePosition.y].type = WALL;
+		//}
+		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+			map.grid[(int)mousePosition.x][(int)mousePosition.y].type = WATER;
+			map.grid[(int)mousePosition.x+1][(int)mousePosition.y].type = WATER;
+			map.grid[(int)mousePosition.x][(int)mousePosition.y+1].type = WATER;
+			map.grid[(int)mousePosition.x+1][(int)mousePosition.y+1].type = WATER;
+			map.grid[(int)mousePosition.x-1][(int)mousePosition.y].type = WATER;
+			map.grid[(int)mousePosition.x-1][(int)mousePosition.y+1].type = WATER;
+			map.grid[(int)mousePosition.x+2][(int)mousePosition.y].type = WATER;
+			map.grid[(int)mousePosition.x+2][(int)mousePosition.y+1].type = WATER;
+			map.grid[(int)mousePosition.x+3][(int)mousePosition.y].type = WATER;
+			map.grid[(int)mousePosition.x+3][(int)mousePosition.y+1].type = WATER;
+		}
+		float delta = GetFrameTime();
+		//map.calculateFrame();
 
 
+		//for (auto& sand : map.sandParticles) {
+		//    DrawPixel(sand.posX, sand.posY, BLACK);
+		//}
 
-        //DrawCircle(posX, posY, 10, RED);
-        EndDrawing();
-    }
+		//DrawCircle(posX, posY, 10, RED);
+
+		map.calculateFrame();
+
+		for (int x = 0; x < 800; x++) {
+			for (int y = 0; y < 600; y++) {
+				if (map.grid[x][y].type == SAND) {
+					DrawPixel(x, y, BLACK);
+				}
+				else if (map.grid[x][y].type == WALL) {
+					DrawPixel(x, y, GRAY);
+				}
+				else if (map.grid[x][y].type == WATER) {
+					DrawPixel(x, y, BLUE);
+				}
+			}
+		}
+
+
+		EndDrawing();
+	}
 
     CloseWindow();
     return 0;
